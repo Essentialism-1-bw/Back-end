9cec456e4ad858719aa4a32d816e33858ca072d6
seongck <seongk@pm.me>
2019-12-23 21:15:50 -0800

    Add values and users-values seeds

---
+++ data/seeds/02-values.js
@@ -0,0 +1,27 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('table_name').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('values').insert([
+        {id: 1, name: 'Athleticism'},
+        {id: 2, name: 'Art'},
+        {id: 3, name: 'Literature'},
+        {id: 4, name: 'Creativity'},
+        {id: 5, name: 'Independence'},
+        {id: 6, name: 'Kindness & Generosity'},
+        {id: 7, name: 'Spirituality'},
+        {id: 8, name: 'Friends'},
+        {id: 9, name: 'Family'},
+        {id: 10, name: 'Music'},
+        {id: 11, name: 'Community'},
+        {id: 12, name: 'Morals'},
+        {id: 13, name: 'Nature'},
+        {id: 14, name: 'Relationships'},
+        {id: 15, name: 'Sense of Humor'},
+        {id: 16, name: 'Career Success'},
+        {id: 17, name: 'Health'}
+      ]);
+    });
+};
---
+++ data/seeds/03-users-values.js
@@ -0,0 +1,16 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('users_values').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users_values').insert([
+        {user_id: 1, value_id: 1 },
+        {user_id: 1, value_id: 4 },
+        {user_id: 1, value_id: 15 },
+        {user_id: 2, value_id: 2 },
+        {user_id: 2, value_id: 8 },
+        {user_id: 2, value_id: 10 }
+      ]);
+    });
+};
