bc58f79db8bf515d6ebb4b6ecec1d775ecc8d7a1
seongck <seongk@pm.me>
2019-12-24 18:36:53 -0800

    Add users-model file

---
+++ models/users-model.js
@@ -0,0 +1,29 @@
+const db = require('../data/dbConfig.js');
+
+async function add(user) {
+  const [ id ] = await db('users').insert(user);
+
+  return db('users')
+    .where({ id })
+    .first();
+}
+
+async function getBy(filter) {
+}
+
+async function getById(id) {
+}
+
+async function getAll() {
+}
+
+async function remove(id) {
+}
+
+module.exports = {
+  add,
+  getBy,
+  getById,
+  getAll,
+  remove
+}
