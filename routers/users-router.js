fe449c10d376d40bd79d220fb037255a14fb2243
seongck <seongk@pm.me>
2019-12-27 03:37:40 -0800

    Add PUT and DELETE endpoint handlers

--- routers/users-router.js
+++ routers/users-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Users = require('../models/users-model.js');
+const bcrypt = require('bcryptjs');
 
 router.get('/', async (req, res, next) => {
   try {
@@ -16,7 +17,7 @@ router.get('/', async (req, res, next) => {
 });
 
 router.get('/:id', async (req, res, next) => {
-  const id = req.params.id;
+  const { id } = req.params;
 
   try {
     const user = await Users.getById(id);
@@ -31,6 +32,45 @@ router.get('/:id', async (req, res, next) => {
   } catch(error) {
     next(error);
   }
+});
+
+router.put('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 12);
+  user.password = hash;
+
+  try {
+    const updatedUser = await Users.update(user, id);
+
+    !updatedUser
+      ? next({
+          status: 404,
+          message: 'Error updating the user'
+        })
+      : res.status(200).json(updatedUser);
+  } catch(error) {
+    next(error);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const result = await Users.remove(id);
+
+    result !== 1
+      ? next({
+          status: 404,
+          message: 'Error deleting the user'
+        })
+      : res.status(200).json({ message: 'User successfully deleted' });
+
+  } catch (error) {
+    next(error);
+  }
+
 });
 
 module.exports = router;
