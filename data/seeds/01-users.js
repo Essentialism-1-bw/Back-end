a788d50f8174300d18b03d0eb7712250276c13f6
seongck <seongk@pm.me>
2019-12-29 20:55:51 -0800

    Remove id properties from seed data

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,12 +1,12 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('users').delete({})
+  return knex('users').delete()
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, email: 'john@email.com', password: 'password', firstName: 'John', lastName: 'Galt', dateOfBirth: '1980-12-01', countryRegion: 'United States' },
-        {id: 2, email: 'mary@email.com', password: 'password', firstName: 'Mary', lastName: 'Anne', dateOfBirth: '1982-11-14', countryRegion: 'United States' }
+        { email: 'john@email.com', password: 'password', firstName: 'John', lastName: 'Galt', dateOfBirth: '1980-12-01', countryRegion: 'United States' },
+        { email: 'mary@email.com', password: 'password', firstName: 'Mary', lastName: 'Anne', dateOfBirth: '1982-11-14', countryRegion: 'United States' }
       ]);
     });
 };
