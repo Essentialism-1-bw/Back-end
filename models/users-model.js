b1f2e36c957b5cc961f7c02d17ae66d3bd80f500
seongck <seongk@pm.me>
2019-12-29 21:16:03 -0800

    Add returning prop for id

--- models/users-model.js
+++ models/users-model.js
@@ -1,7 +1,7 @@
 const db = require('../data/dbConfig.js');
 
 async function add(user) {
-  const [ id ] = await db('users').insert(user);
+  const [ id ] = await db('users').returning('id').insert(user);
 
   return db('users')
     .where({ id })
