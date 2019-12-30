6bf0d115296e531002913e2399457e9002a130de
seongck <seongk@pm.me>
2019-12-29 16:41:51 -0800

    Change settings for PostgreSQL

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,8 @@
 // Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
-
+  /*
   development: {
     client: 'sqlite3',
     connection: {
@@ -27,6 +28,51 @@ module.exports = {
     seeds: {
       directory: './data/seeds',
     }
-  }
+  } 
+  */
+  development: {
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      user: 'postgres',
+      password: process.env.POSTGRES_PASSWORD,
+      database: 'essentialism'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    }
+  },
 
+  testing: {
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      user: 'postgres',
+      password: process.env.POSTGRES_PASSWORD,
+      database: 'essentialism_test'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    }
+  }
 };
