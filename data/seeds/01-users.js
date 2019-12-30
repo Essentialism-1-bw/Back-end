4c555f12df180b9cabb21209e2925cecd785afdc
seongck <seongk@pm.me>
2019-12-29 22:00:35 -0800

    Hash seed data passwords

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
@@ -5,8 +6,21 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        { email: 'john@email.com', password: 'password', firstName: 'John', lastName: 'Galt', dateOfBirth: '1980-12-01', countryRegion: 'United States' },
-        { email: 'mary@email.com', password: 'password', firstName: 'Mary', lastName: 'Anne', dateOfBirth: '1982-11-14', countryRegion: 'United States' }
+        { 
+          email: 'john@email.com',
+          password: bcrypt.hashSync('password', 12),
+          firstName: 'John',
+          lastName: 'Galt',
+          dateOfBirth: '1980-12-01',
+          countryRegion: 'United States'
+        },
+        { email: 'mary@email.com',
+          password: bcrypt.hashSync('password', 12),
+          firstName: 'Mary',
+          lastName: 'Anne',
+          dateOfBirth: '1982-11-14',
+          countryRegion: 'United States'
+        }
       ]);
     });
 };
