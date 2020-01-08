62a1333784cf5c0ed60b42783236d95b49621b80
jackskim <jackskim29@gmail.com>
2020-01-07 19:14:08 -0800

    Return id upon login

--- routers/auth-router.js
+++ routers/auth-router.js
@@ -31,6 +31,7 @@ router.post('/login', (req, res, next) => {
       if( user && bcrypt.compareSync(password, user.password)) {
         const token = genToken(user);
         res.status(200).json({
+          id: user.id,
           message: `Welcome ${user.firstName}`,
           token: token
         });
