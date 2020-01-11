965bc30422755807f61b31b52c66439cbffbe136
jackskim <jackskim29@gmail.com>
2020-01-10 19:50:51 -0800

    Add authentication for loggedin user id === endpoint id

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,11 +3,14 @@ const secret = require('../config/secret.js');
 
 module.exports = (req, res, next) => {
   const token = req.headers.authorization;
+  const { id } = req.params;
 
   if(token) {
     jwt.verify(token, secret.jwtSecret, (err, decodedToken) => {
-      if(err) {
-        res.status(401).json({ message: 'Access denied.  Please log in.' });
+      if(err ) {
+        res.status(401).json({ message: 'Access denied. Please log in.' });
+      } else if( +id !== decodedToken.userid && id !== undefined ){
+        res.status(401).json({ message: 'Access denied.' });
       } else {
         req.decodedJwt = decodedToken;
         next();
